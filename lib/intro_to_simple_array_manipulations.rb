1efa23e05b114dae178abd8fae256dc3cced2433
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 03:42:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,4 +21,9 @@ end
 def using_shift(array)
   @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   @im_so_over_this_city = @my_favorite_cities.shift
+end
+
+def shift_with_args(array)
+  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  @brands_removed = @ice_cream_brands.shift(2)
 end  
