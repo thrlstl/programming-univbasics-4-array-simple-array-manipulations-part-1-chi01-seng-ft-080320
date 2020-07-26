8980de426e3784522527e8360ac34131e469c537
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 03:41:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,6 +14,6 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-  dog_breeds.pop(2)
+  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  @small_dogs = @dog_breeds.pop(2)
 end
